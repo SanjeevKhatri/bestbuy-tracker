19d268276ae82410f50a0798134fbeb8711e5941
sanjeevkhatri <sanjeevkhatri29@gmail.com>
2025-05-06 19:07:16 -0500

    Switch to render.yaml for proper Playwright setup

--- render-build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Use virtual environment Python if available
-VENV_PY=$(which python)
-
-# Install Playwright and browser dependencies
-$VENV_PY -m playwright install --with-deps
